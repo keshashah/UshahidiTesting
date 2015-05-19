5c871a9369b99b8c3b4c03372bab3c178265dd60
KeshaShah <kesha.shah1106@gmail.com>
2015-05-20 01:28:47 +0530

    Making driver instance

---
+++ src/com/ushahidi/pages/DriverInstance.java
@@ -0,0 +1,13 @@
+package com.ushahidi.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverInstance {
+	private WebDriver webDriver;
+	
+	public DriverInstance(WebDriver webdriver)
+	{
+		super();
+		this.webDriver = webdriver;
+	}
+}
